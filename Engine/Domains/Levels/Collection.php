60a095a927f8a0762b98a318c0bf71c4c58b5a37
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-07-16 12:27:35 +0300

    [0.01.007] Add levels domain.

---
+++ Engine/Domains/Levels/Collection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Liloi\BabylonV\Domains\Levels;
+
+use Liloi\Tools\Collection as AbstractCollection;
+
+/**
+ * @package Engine\Domain\Exercise
+ */
+class Collection extends AbstractCollection
+{
+}
