0fbcd3e444d35b746be89b9010d1560fe23215de
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-07-17 06:42:46 +0300

    [0.01.014] Add testing systems (phpUnit, Jasmine).

---
+++ Tests/Helper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Liloi\BabylonV;
+
+use Liloi\Config\Pool;
+use Liloi\Config\Sparkle;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Check phpUnit testing ability.
+ */
+class Helper
+{
+    public static function truncateDatabase(): void
+    {
+        $prefix = 'tardis_';
+        $tables = [
+            $prefix . 'road',
+            $prefix . 'config',
+            $prefix . 'logs'
+        ];
+        $connection = Pool::getSingleton()->get('connection');
+        $db = \mysqli_connect(
+            $connection['host'],
+            $connection['user'],
+            $connection['password'],
+            $connection['database'],
+        );
+
+        foreach ($tables as $table)
+        {
+            $db->query(sprintf('truncate table %s', $table));
+        }
+    }
+}
